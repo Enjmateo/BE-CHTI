a6844588727edf55adb49564054601e9c28d0230
Enjmateo <enjmateo@yahoo.fr>
2021-04-06 16:23:31 +0200

    Modififications et mise à léchelle StepSon

--- PjtKEIL_StepSon/Src/GestionSon.s
+++ PjtKEIL_StepSon/Src/GestionSon.s
@@ -25,7 +25,6 @@ AdresseSon dcd 0
 
 
 CallbackSon proc 
-	push {lr}
 	ldr r0,=AdresseSon ;On stocke l'adresse de AdresseSon dans r0
 	ldr r3,=SortieSon ;On stocke l'adresse de Sortie dans r3
 	
@@ -38,12 +37,20 @@ CallbackSon proc
 	;Dans le cas ou c'est différent de 0 on continue
 SuiteCallback
 	;on incrémente la valeur de adresseson de 16bits (2octets)
+	
 	add r1,#2
 	str r1,[r0] ;on la stocke dans adresseson
-	ldr r1,[r1] ;on récupère la valeur à la nouvelle adresse
+	ldrsh r1,[r1] ;on récupère la valeur à la nouvelle adresse
+	 ;ldr r1,[r0],#2
+	mov r2, #45
+	mul r1,r2
+	mov r2, #4096
+	sdiv r1, r2
+	adds r1, #360
 	str r1,[r3] ;on la mets dans sortie son
 	;ldr SortieSon,[r1]
-	pop {pc}
+	bx lr;
+
 indexzero
 	ldr r2,=Son
 	str r2,[r0]
